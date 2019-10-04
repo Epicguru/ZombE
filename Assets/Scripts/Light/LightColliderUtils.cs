e8ba8560d7df16484ae0d29a6b4fdf19464e6f2a
James Billy <james@epicguru.co.uk>
2019-10-04 19:19:23 +0200

    Switched to 2D renderer.

---
+++ Assets/Scripts/Light/LightColliderUtils.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZombE
+{
+    public static class LightColliderUtils
+    {
+        private static List<Vector2> tempPoints = new List<Vector2>();
+        public static List<Vector2> GetVertices(this CompositeCollider2D collider)
+        {
+            tempPoints.Clear();
+
+            Debug.Log($"Path count: {collider.pathCount}");
+            Debug.Log($"Total point count: {collider.pointCount}");
+
+            for (int i = 0; i < collider.pathCount; i++)
+            {
+                int x = collider.GetPath(i, tempPoints);
+                Debug.Log($"Path {i} has {collider.GetPathPointCount(i)} points; x = {x}");
+            }
+
+            return tempPoints;
+        }
+    }
+}
---
+++ Assets/Scripts/PawnMovement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+namespace ZombE
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PawnMovement : MonoBehaviour
+    {
+        public Rigidbody2D Body
+        {
+            get
+            {
+                if (_body == null)
+                    _body = GetComponent<Rigidbody2D>();
+                return _body;
+            }
+        }
+        private Rigidbody2D _body;
+
+        public Vector2 Direction;
+        public float Speed = 7f;
+
+        public float InputMagnitude = 100f;
+
+        private Vector2 startPos;
+
+        private void Update()
+        {
+            if (!Application.isMobilePlatform)
+            {
+                Direction = new Vector2();
+                Direction.x = Input.GetKey(KeyCode.D) ? 1 : Input.GetKey(KeyCode.A) ? -1 : 0;
+                Direction.y = Input.GetKey(KeyCode.W) ? 1 : Input.GetKey(KeyCode.S) ? -1 : 0;
+            }
+            else
+            {
+                Direction = new Vector2();
+                if (Input.GetMouseButtonDown(0))
+                {
+                    startPos = Input.mousePosition;
+                }
+                if (Input.GetMouseButton(0))
+                {
+                    Vector2 delta = (Vector2)Input.mousePosition - startPos;
+
+                    Direction = delta.normalized * (Mathf.Clamp01(delta.magnitude / InputMagnitude));
+                }
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 dir = Direction;
+            if(Direction.sqrMagnitude > 1f)
+            {
+                dir = Direction.normalized;
+            }
+
+            Body.velocity = dir * Speed;
+        }
+    }
+}
