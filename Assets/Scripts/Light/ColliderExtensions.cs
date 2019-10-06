30d116800e9f0c150a8006c56f9d99b8406b88ba
James Billy <james@epicguru.co.uk>
2019-10-06 22:45:21 +0200

    Wasting time on 2d lights...

---
+++ Assets/Scripts/Light/ColliderExtensions.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZombE
+{
+    public static class ColliderExtensions
+    {
+        private static List<Vector2> path = new List<Vector2>();
+
+        /// <summary>
+        /// Gets the relevant edge points on a collider surface, when viewed from a particulat point.
+        /// Does not occlude points, the view point is just used for circle colliders.
+        /// </summary>
+        /// <param name="collider">The collider to get edge points from. Not all 2D collider types are supported.</param>
+        /// <param name="castPoint">The point from which the collider is 'viewed'. Only used for circle colliders.</param>
+        /// <param name="points">A list of points. Must not be null. Points are added on to the end of the list.</param>
+        /// <returns>The number of points added.</returns>
+        public static int GetCastPoints(this Collider2D collider, Vector2 castPoint, List<Vector2> points)
+        {
+            if (collider == null)
+                return 0;
+
+            if (points == null)
+                return 0;
+
+            int startCount = points.Count;
+
+            var type = collider.GetType();
+            var name = type.Name;
+            switch (name)
+            {
+                case "BoxCollider2D":
+
+                    BoxCollider2D box = collider as BoxCollider2D;
+
+                    Vector2 topLeft = box.transform.TransformPoint(box.offset + new Vector2(box.size.x * -0.5f, box.size.y * 0.5f));
+                    points.Add(topLeft);
+
+                    Vector2 topRight = box.transform.TransformPoint(box.offset + new Vector2(box.size.x * 0.5f, box.size.y * 0.5f));
+                    points.Add(topRight);
+
+                    Vector2 bottomLeft = box.transform.TransformPoint(box.offset + new Vector2(box.size.x * -0.5f, box.size.y * -0.5f));
+                    points.Add(bottomLeft);
+
+                    Vector2 bottomRight = box.transform.TransformPoint(box.offset + new Vector2(box.size.x * 0.5f, box.size.y * -0.5f));
+                    points.Add(bottomRight);
+
+                    break;
+
+                case "EdgeCollider2D":
+
+                    EdgeCollider2D edge = collider as EdgeCollider2D;
+
+                    foreach (var point in edge.points)
+                    {
+                        Vector2 worldPoint = edge.transform.TransformPoint(point.x, point.y, 0f);
+                        points.Add(worldPoint);
+                    }
+
+                    break;
+
+                case "CircleCollider2D":
+
+                    const float POINTS_PER_QUADRANT = 1f;
+                    const float RADIUS_MULTI = Mathf.PI * 2f;
+
+                    CircleCollider2D circle = collider as CircleCollider2D;
+
+                    Vector2 center = circle.transform.TransformPoint(circle.offset);
+
+                    // r = the circle radius. Uses a scale aproximation.
+                    float r = circle.radius * circle.transform.lossyScale.x;
+                    if ((castPoint - center).sqrMagnitude <= r * r)
+                        break;
+
+                    float dst = (castPoint - center).magnitude;
+
+                    float tangentChangeAngle = Mathf.Acos(r / dst);
+
+                    float angleFromCenterToP = Mathf.Atan2(castPoint.y - center.y, castPoint.x - center.x);
+
+                    float t1Angle = angleFromCenterToP + tangentChangeAngle;
+                    float t2Angle = angleFromCenterToP - tangentChangeAngle;
+
+                    Vector2 t1Offset = new Vector2(Mathf.Cos(t1Angle), Mathf.Sin(t1Angle)) * r;
+                    Vector2 t2Offset = new Vector2(Mathf.Cos(t2Angle), Mathf.Sin(t2Angle)) * r;
+                    Vector2 t1 = center + t1Offset;
+                    Vector2 t2 = center + t2Offset;
+
+                    points.Add(t1);
+
+                    float angleDiff = Vector2.Angle(t1Offset, t2Offset);
+                    int extraPointCount = Mathf.CeilToInt(POINTS_PER_QUADRANT * (angleDiff / 90f) * (RADIUS_MULTI * r));
+                    for (int i = 0; i < extraPointCount; i++)
+                    {
+                        float p = (float)(i + 0.5f) / extraPointCount;
+                        float angle = Mathf.Lerp(t1Angle, t2Angle, p);
+                        Vector2 tInter = center + new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * r;
+                        points.Add(tInter);
+                    }
+
+                    points.Add(t2);
+
+                    break;
+
+                case "PolygonCollider2D":
+
+                    PolygonCollider2D poly = collider as PolygonCollider2D;
+                    var trs = poly.transform;
+                    path.Clear();
+
+                    int pathCount = poly.pathCount;
+                    for (int i = 0; i < pathCount; i++)
+                    {
+                        int pathLength = poly.GetPath(i, path);
+                        for (int j = 0; j < pathLength; j++)
+                        {
+                            Vector2 point = path[j];
+                            points.Add(trs.TransformPoint(point));
+                        }
+                    }
+
+                    break;
+
+                case "CompositeCollider2D":
+
+                    CompositeCollider2D comp = collider as CompositeCollider2D;
+                    trs = comp.transform;
+                    path.Clear();
+
+                    pathCount = comp.pathCount;
+                    for (int i = 0; i < pathCount; i++)
+                    {
+                        int pathLength = comp.GetPath(i, path);
+                        for (int j = 0; j < pathLength; j++)
+                        {
+                            Vector2 point = path[j];
+                            points.Add(trs.TransformPoint(point));
+                        }
+                    }
+
+                    break;
+
+                default:
+                    //Debug.LogWarning($"Collider of type {name} ({type.FullName}) is not supported yet.");
+                    break;
+            }
+
+            return points.Count - startCount;
+        }
+
+        /// <summary>
+        ///  Gets the world space center of the collider2D.
+        /// </summary>
+        public static Vector2 GetCentre(this Collider2D collider)
+        {
+            var type = collider.GetType();
+            var name = type.Name;
+            switch (name)
+            {
+                default:
+                    return collider.transform.TransformPoint(collider.offset);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Light/PointLight2D.cs
@@ -0,0 +1,362 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+namespace ZombE
+{
+    public class PointLight2D : MonoBehaviour
+    {
+        private static List<Vector2> tempPoints = new List<Vector2>();
+        private static List<Vector2> visiblePoints = new List<Vector2>();
+        private static SortedList<float, Vector3> finalPoints = new SortedList<float, Vector3>();
+        private static Dictionary<Vector2, Collider2D> pointOwnership = new Dictionary<Vector2, Collider2D>();
+        private static Dictionary<Collider2D, float> colliderAngles = new Dictionary<Collider2D, float>();
+        private static CircleCollider2D CircleCollider
+        {
+            get
+            {
+                if(_cc == null)
+                {
+                    var go = new GameObject("Temp Circle Collider");
+                    _cc = go.AddComponent<CircleCollider2D>();
+                    _cc.radius = _cr;
+                }
+                return _cc;
+            }
+        }
+        private static CircleCollider2D _cc;
+        private static float _cr;
+
+        public Collider2D[] Colliders;
+        public Light2D Light;
+
+        [Header("Properties")]
+        public float Radius = 10f;
+        public Color Color = Color.white;
+
+        [Header("Performace")]
+        [Range(0, 120)]
+        public int AproximationCastCount = 0;
+
+        [Header("Graphics")]
+        public Texture2D LightCookie;
+
+        [Header("Stability")]
+        public float CircleRadius = 0.04f; // 4cm
+        public float EdgeDetectRayOffset = 0.002f; // 1mm
+        public EdgeDetectionMode EdgeDetection = PointLight2D.EdgeDetectionMode.Raycast;
+
+        [Header("Debugging")]
+        public DebugDrawMode DebugMode = 0;
+        public bool QueriesStartInColliders = true;
+
+        private GameObject tempColliderObject;
+
+        public enum DebugDrawMode
+        {
+            None = 0,
+            AllPoints = 1,
+            VisiblePoints = 2,
+            FinalPoints = 4
+        }
+
+        public enum EdgeDetectionMode
+        {
+            Raycast,
+            InteriorPoint
+        }
+
+        private void Awake()
+        {
+            _cr = CircleRadius;
+
+            // Needs to be turned off. Should already be disabled by default in new unity versions.
+            Physics2D.autoSyncTransforms = false;
+        }
+
+        private void LateUpdate()
+        {
+            Debug.Log(Colliders[0].OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+
+            // Basic state checks.
+            if (Colliders == null)
+                return;
+
+            // Clear point lists.
+            visiblePoints.Clear();
+            tempPoints.Clear();
+            finalPoints.Clear();
+            pointOwnership.Clear();
+            colliderAngles.Clear();
+
+            // Update colliders positions.
+            Physics2D.SyncTransforms();
+            Physics2D.queriesStartInColliders = false;
+
+            // TODO add more edge points and make edge points 'slide' towards the source when blocked.
+            // Add the static 4 edge points.
+            float rp = Radius + 1; // A bit further than the real range.
+            tempPoints.Add(transform.position + new Vector3(-rp, rp));
+            tempPoints.Add(transform.position + new Vector3(rp, rp));
+            tempPoints.Add(transform.position + new Vector3(rp, -rp));
+            tempPoints.Add(transform.position + new Vector3(-rp, -rp));
+
+            // Occlude and then add the corners.
+            int cornersAdded = 0;
+            foreach (var point in tempPoints)
+            {
+                CircleCollider.offset = point;
+                var hit = Physics2D.Linecast(transform.position, point);
+                if (hit.collider == CircleCollider)
+                {
+                    visiblePoints.Add(point);
+                    cornersAdded++;
+                }
+                else
+                {
+                    visiblePoints.Add(hit.point);
+                    cornersAdded++;
+                }
+            }
+            tempPoints.Clear();
+
+            bool drawAllPoints = DebugMode.HasFlag(DebugDrawMode.AllPoints);
+
+            // Get all edge vertices from all colliders...
+            foreach (var collider in Colliders)
+            {
+                if (collider == null)
+                    continue;
+
+                // Work out the angle to the centre of the collider, and store it.
+                Vector2 colliderCentre = collider.GetCentre();
+                Vector2 centreDiff = colliderCentre - (Vector2)transform.position;
+                colliderAngles.Add(collider, Mathf.Atan2(centreDiff.y, centreDiff.x));
+
+                // Get the points for this collider.
+                collider.GetCastPoints(transform.position, tempPoints);
+
+                // Raycast to all vertices.
+                for (int i = 0; i < tempPoints.Count; i++)
+                {
+                    Vector2 point = tempPoints[i];
+
+                    // Debug draw.
+                    if (drawAllPoints)
+                    {
+                        Debug.DrawLine(transform.position, point, Color.green);
+                    }
+
+                    // Remove all points out of range.
+                    float sqrDst = (point - (Vector2)transform.position).sqrMagnitude;
+                    if (sqrDst > Radius * Radius)
+                    {
+                        continue;
+                    }
+
+                    CircleCollider.offset = point;
+                    var hit = Physics2D.Linecast(transform.position, point);
+                    if (hit && hit.collider == CircleCollider)
+                    {
+                        visiblePoints.Add(point);
+
+                        // Record point ownership.
+                        pointOwnership.Add(point, collider);
+                    }
+                }
+
+                // Clear the points list after every collider.
+                tempPoints.Clear();
+            }
+
+            CircleCollider.offset = new Vector2(10000f, 10000f); // Out of sight, out of mind.        
+
+            Physics2D.queriesStartInColliders = true;
+
+            // Check which visible points are edges...
+            for (int i = 0; i < visiblePoints.Count; i++)
+            {
+                Vector2 point = visiblePoints[i];
+
+                // If this point is a corner, skip edge detection. It isn't an edge because it is a floating point!
+                if (i < cornersAdded)
+                {
+                    AddFinalPoint(point);
+                    continue;
+                }
+
+                Vector2 dir = (point - (Vector2)transform.position);
+                Vector2 dirNormalized = dir.normalized;
+                Vector2 start = point + dirNormalized * EdgeDetectRayOffset;
+
+                const float NUDGE = 0.0001f;
+
+                var hit = Physics2D.Raycast(start, dir, Radius); // Note that the range value here is just to avoid calculating the remaining distance.
+                LogRaycast(start, hit);
+
+                bool inCollider = false;
+                if (EdgeDetection == EdgeDetectionMode.InteriorPoint)
+                    inCollider = pointOwnership[point].OverlapPoint(start);
+
+                bool airBehind = EdgeDetection == EdgeDetectionMode.Raycast ? !hit : !inCollider;
+
+                if (airBehind)
+                {
+                    // It's an edge.
+                    // There is nothing behind it.
+                    // Add a new point that is behind it.
+
+                    bool clockwise = IsClockwise(point);
+                    AddFinalPoint((Vector2)transform.position + dirNormalized * Radius * 1.2f, clockwise ? -NUDGE : NUDGE);
+                }
+                else
+                {
+                    if (EdgeDetection == EdgeDetectionMode.Raycast ? hit.point == start : inCollider)
+                    {
+                        // Can't go in there!
+                        // No extra point added here.
+                    }
+                    else
+                    {
+                        // It's an edge, with something behind it.
+                        // Add a new point on whatever it hit behind.
+                        bool clockwise = IsClockwise(point);
+                        AddFinalPoint(hit.point, clockwise ? -NUDGE : NUDGE);
+                    }
+                }
+
+                AddFinalPoint(point);
+            }
+
+            Physics2D.queriesStartInColliders = false;
+            // Do aproximation casts.
+            // This is simply firing rays outwards in all directions, and adding any point that they hit.
+            // Makes mesh generation and rendering much slower, but can help avoid occluded vertices creating incorrect lighting.
+            // By default, no rays are casted, but up to 120 per frame are allowed.
+            for (int i = 0; i < AproximationCastCount; i++)
+            {
+                float p = (float)i / AproximationCastCount;
+                float angle = p * Mathf.PI * 2f;
+
+                Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+
+                var hit = Physics2D.Raycast(transform.position, direction, Radius);
+
+                // If we hit something, add that point.
+                if (hit)
+                {
+                    AddFinalPoint(hit.point);
+                }
+
+            }
+
+            DrawDebug(tempPoints);
+
+            var array = finalPoints.Values.ToArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i] - transform.position;
+            }
+            SetPoints(array);
+        }
+
+        private void SetPoints(Vector3[] array)
+        {
+            if (array == null || array.Length < 3)
+            {
+                Debug.LogWarning($"Tried to set invalid array. Length: {array?.Length.ToString() ?? "(null array)"}");
+                return;
+            }
+            Light.GetType().GetField("m_ShapePath", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(Light, array);
+        }
+
+        private bool IsClockwise(Vector2 point)
+        {
+            Collider2D collider = pointOwnership[point];
+            Debug.Assert(collider != null);
+
+            Vector2 diffToPoint = point - (Vector2)transform.position;
+
+            float centreAngle = colliderAngles[collider] * Mathf.Rad2Deg;
+            float pointAngle = Mathf.Atan2(diffToPoint.y, diffToPoint.x) * Mathf.Rad2Deg;
+
+            float net = Mathf.DeltaAngle(centreAngle, pointAngle);
+
+            bool isClockwise = net < 0f;
+
+            return isClockwise;
+        }
+
+        private void AddFinalPoint(Vector2 point, float angleOffset = 0f)
+        {
+            float angle = CalcAngle(point) + angleOffset;
+            while (finalPoints.ContainsKey(angle))
+            {
+                angle += 0.00001f; // Since the two points are in the same place, it doesn't really matter which forms the triangle.
+            }
+
+            finalPoints.Add(angle, point);
+        }
+
+        private float CalcAngle(Vector2 point)
+        {
+            Vector2 diff = point - (Vector2)transform.position;
+            return Mathf.Atan2(diff.y, diff.x);
+        }
+
+        private void DrawDebug(List<Vector2> allPoints)
+        {
+            if (DebugMode.HasFlag(DebugDrawMode.VisiblePoints))
+            {
+                foreach (var point in visiblePoints)
+                {
+                    Debug.DrawLine(transform.position, point, Color.magenta);
+                }
+            }
+            if (DebugMode.HasFlag(DebugDrawMode.FinalPoints))
+            {
+                int i = 0;
+                foreach (var pair in finalPoints)
+                {
+                    bool first = i == 0;
+                    bool last = i == finalPoints.Count - 1;
+
+                    Vector2 point = pair.Value;
+                    const float SIZE = 0.03f;
+                    Color c = first ? Color.green : last ? Color.red : Color.yellow;
+                    Debug.DrawLine(point + new Vector2(-SIZE, SIZE), point + new Vector2(SIZE, -SIZE), c);
+                    Debug.DrawLine(point + new Vector2(-SIZE, -SIZE), point + new Vector2(SIZE, SIZE), c);
+
+                    i++;
+                }
+            }
+        }
+
+        private void LogRaycast(Vector2 origin, RaycastHit2D hit)
+        {
+            if (hit)
+            {
+                Debug.DrawLine(origin, hit.point, Color.red);
+                Debug.DrawLine(hit.point, hit.point + hit.normal * 0.1f, Color.cyan);
+            }
+            else
+            {
+                Debug.DrawLine(origin, origin + Vector2.up * 0.2f, Color.magenta);
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            float oldA = Color.a;
+            Color.a = 0.3f;
+            Gizmos.color = Color;
+            Color.a = oldA;
+
+            Gizmos.DrawWireSphere(transform.position, Radius);
+
+        }
+    }
+}
+
